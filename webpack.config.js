20d357888e32b6fd71c056c7122999d3d0c96d81
mingquan.li <mingquan.li@5262.com>
2018-04-16 15:25:12 +0800

    使用webpack构建本地服务器

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+	devtool: "eval-source-map",//使用eval打包源文件模块，在同一个文件中生成干净的完整的source map
+
 	entry: __dirname + "/app/main.js",//唯一入口文件
 	output: {
 		path: __dirname + "/public",//打包后的文件存放的地方
 		filename: "bundle.js"//打包后输出文件的文件名
+	},
+
+	devServer: {
+		contentBase: "./public",//本地服务器所加载的页面所在的目录
+    		historyApiFallback: true,//不跳转
+    		inline: true//实时刷新
 	}
 }
