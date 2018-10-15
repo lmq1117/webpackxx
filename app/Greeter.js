61b292742da539da9ce43c7a0f41dcb48b330712
limq <limq@cgnfs.com.cn>
2018-10-15 10:35:30 +0800

    分割线============

--- app/Greeter.js
+++
@@ -1,7 +0,0 @@
-// CommonJS规范导出这个函数为一个模块
-//Greeter.js
-module.exports = function(){
-	var greet = document.createElement('div');
-	greet.textContent = "Hi Everyone and Greetings!";
-	return greet;
-}
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.export = {
-	entry: __dirname + "/app/main.js",
-	output: {
-		path: __dirname + "/public",
-		filename: "bundle.js"
-	}
-}
