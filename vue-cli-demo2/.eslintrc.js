632e91f507bca9b9fe1fbaf8785f137c4ca07b83
limq <limq@cgnfex.com>
2021-03-10 08:55:18 +0800

    vueclidemo12

---
+++ vue-cli-demo2/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    // semi: [1, "never"], // 禁止用分号
+    indent: [2, 2], // 缩进采用2个空格
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
---
+++ vue-cli-demo2/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    port: 8008,
+    open: true,
+    https: false,
+    host: "localhost"
+  },
+  lintOnSave: true,
+  outputDir: "dist2",
+  assetsDir: "assets2",
+  indexPath: "out/index.html",
+  productionSourceMap: false
+};
